e5615bcded20d49b2710515868d8a9215144ff76
Priscilla Jospin <priscillajospin@karunya.edu.in>
2025-07-23 23:21:06 +0530

    Fix vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Very important for Netlify
+  base: '/', // VERY IMPORTANT for Netlify
 });
