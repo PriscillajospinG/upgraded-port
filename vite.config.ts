4b9c30338807a3dfc1f0e92bf78ccef5737932da
Priscilla Jospin <priscillajospin@karunya.edu.in>
2025-07-24 11:41:22 +0530

    logo and image changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // VERY IMPORTANT for Netlify
+  base: './', // VERY IMPORTANT for Netlify
 });
